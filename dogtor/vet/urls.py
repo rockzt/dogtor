b6653078834a8a6588d1cbd81fa65fa503764168
Rodrigo Zavala <rodrigoztrejo@gmail.com>
2023-09-18 16:32:44 -0600

    Minor changes

--- dogtor/vet/urls.py
+++ dogtor/vet/urls.py
@@ -11,15 +11,15 @@ from .views import list_pet_owners, Test, OwnersList, Home, OwnersDetail, PetsLi
 # If you want to access  "vet:owners_list"
 urlpatterns = [
     path('', Home.as_view(),name="home"),
-    path('owners/', OwnersList.as_view(), name="owners_list"), # Configuring generic class view, Listview, renders template, include and alias  "owners_list", this is the sintaxis when you do not posess include
+    path('owners', OwnersList.as_view(), name="owners_list"), # Configuring generic class view, Listview, renders template, include and alias  "owners_list", this is the sintaxis when you do not posess include
     path('owners/<int:pk>', OwnersDetail.as_view(), name="owners_detail"), # Configuring generic class view, DetailView, renders template, include and alias  "owners_list"
-    path('owners/add/', OwnersCreate.as_view(), name="owners_create"), # Configuring CreateView of owners
-    path('owners/<int:pk>/edit/', OwnersUpdate.as_view(), name="owners_edit"), # Configuring UpdateView, setting route using pk to locate what owner we want to edit
-    path('owners/<int:pk>/delete/', OwnersDelete.as_view(), name="owners_delete"), # Configuring DeleteView, setting route using pk to locate what owner we want to delete
-    path('pets/add/', PetsCreate.as_view(), name="pets_create"), # Configuring CreateView of Pets
-    path('pets/<int:pk>/delete/', PetsDelete.as_view(), name="pets_delete"), # Configuring DeleteView, setting route using pk to locate what owner we want to delete
-    path('pets/<int:pk>/edit/', PetsUpdate.as_view(), name="pets_edit"), # Configuring CreateView of Pets
-    path('pets/', PetsList.as_view(), name="pets_list"), # Configuring generic class view, renders template
+    path('owners/add', OwnersCreate.as_view(), name="owners_create"), # Configuring CreateView of owners
+    path('owners/<int:pk>/edit', OwnersUpdate.as_view(), name="owners_edit"), # Configuring UpdateView, setting route using pk to locate what owner we want to edit
+    path('owners/<int:pk>/delete', OwnersDelete.as_view(), name="owners_delete"), # Configuring DeleteView, setting route using pk to locate what owner we want to delete
+    path('pets/add', PetsCreate.as_view(), name="pets_create"), # Configuring CreateView of Pets
+    path('pets/<int:pk>/delete', PetsDelete.as_view(), name="pets_delete"), # Configuring DeleteView, setting route using pk to locate what owner we want to delete
+    path('pets/<int:pk>/edit', PetsUpdate.as_view(), name="pets_edit"), # Configuring CreateView of Pets
+    path('pets', PetsList.as_view(), name="pets_list"), # Configuring generic class view, renders template
     path('pets/<int:pk>', PetDetail.as_view(), name="pets_detail"), # Configuring generic class view, renders template
-    path('test/', Test.as_view()), # Configuring generic class view, this class does not render template
+    path('test', Test.as_view()), # Configuring generic class view, this class does not render template
 ]
