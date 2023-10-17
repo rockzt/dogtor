87cc73cb82f23c4d56342f42b627df9cae7e170d
Restyled.io <commits@restyled.io>
2023-10-17 06:11:04 +0000

    Restyled by yapf

--- dogtor/vet/forms.py
+++ dogtor/vet/forms.py
@@ -11,6 +11,7 @@ from .models import PetOwner
 
 class OwnerForm(forms.ModelForm):
     """ """
+
     # 1.- Model relate to current form
     # 2.- Fields presented on the current form
 
@@ -19,107 +20,101 @@ class OwnerForm(forms.ModelForm):
         model = PetOwner  # 1
         fields = ["first_name", "last_name", "address", "email", "phone"]
         widgets = {
-            "first_name": forms.TextInput(
-                attrs={
-                    "type": "text",
-                    "class": "form-control",
-                }
-            ),
-            "last_name": forms.TextInput(
-                attrs={
-                    "type": "text",
-                    "class": "form-control",
-                }
-            ),
-            "address": forms.TextInput(
-                attrs={
-                    "class": "form-control",
-                    "type": "text",
-                }
-            ),
-            "email": forms.EmailInput(
-                attrs={
-                    "class": "form-control",
-                    "type": "email",
-                }
-            ),
-            "phone": forms.NumberInput(
-                attrs={
-                    "class": "form-control",
-                    "type": "number",
-                }
-            ),
+            "first_name":
+            forms.TextInput(attrs={
+                "type": "text",
+                "class": "form-control",
+            }),
+            "last_name":
+            forms.TextInput(attrs={
+                "type": "text",
+                "class": "form-control",
+            }),
+            "address":
+            forms.TextInput(attrs={
+                "class": "form-control",
+                "type": "text",
+            }),
+            "email":
+            forms.EmailInput(attrs={
+                "class": "form-control",
+                "type": "email",
+            }),
+            "phone":
+            forms.NumberInput(attrs={
+                "class": "form-control",
+                "type": "number",
+            }),
         }
 
 
 class PetForm(forms.ModelForm):
     """ """
+
     class Meta:
         """ """
         model = Pet
         fields = ["name", "type", "owner"]
         widgets = {
-            "name": forms.TextInput(
-                attrs={
-                    "type": "text",
-                    "class": "form-control",
-                }
-            ),
-            "type": forms.Select(
-                attrs={
-                    "type": "text",
-                    "class": "form-control",
-                }
-            ),
-            "owner": forms.Select(
-                attrs={
-                    "class": "form-control",
-                    "type": "text",
-                }
-            ),
+            "name":
+            forms.TextInput(attrs={
+                "type": "text",
+                "class": "form-control",
+            }),
+            "type":
+            forms.Select(attrs={
+                "type": "text",
+                "class": "form-control",
+            }),
+            "owner":
+            forms.Select(attrs={
+                "class": "form-control",
+                "type": "text",
+            }),
         }
 
 
 class PetdateForm(forms.ModelForm):
     """ """
+
     class Meta:
         """ """
         model = PetDate
         fields = ["datetime", "type", "pet"]
         widgets = {
-            "datetime": forms.DateTimeInput(
-                attrs={
-                    "class": "form-control",
-                    "type": "datetime-local",
-                }
-            ),
-            "type": forms.Select(
-                attrs={
-                    "class": "form-control",
-                    "type": "text",
-                }
-            ),
-            "pet": forms.Select(
-                attrs={
-                    "class": "form-control",
-                    "type": "text",
-                }
-            ),
+            "datetime":
+            forms.DateTimeInput(attrs={
+                "class": "form-control",
+                "type": "datetime-local",
+            }),
+            "type":
+            forms.Select(attrs={
+                "class": "form-control",
+                "type": "text",
+            }),
+            "pet":
+            forms.Select(attrs={
+                "class": "form-control",
+                "type": "text",
+            }),
         }
 
 
 class SearchForm(forms.Form):
     """ """
-    search_query = forms.CharField(max_length=50, required=False, label="Search")
+    search_query = forms.CharField(max_length=50,
+                                   required=False,
+                                   label="Search")
 
     def __init__(self, *args, **kwargs):
         super(SearchForm, self).__init__(*args, **kwargs)
 
         # Add attributes to the search_query field
-        self.fields["search_query"].widget.attrs.update(
-            {
-                "class": "form-control me-2r",
-                "type": "search",
-                "placeholder": "Enter a name...",
-            }
-        )
+        self.fields["search_query"].widget.attrs.update({
+            "class":
+            "form-control me-2r",
+            "type":
+            "search",
+            "placeholder":
+            "Enter a name...",
+        })
